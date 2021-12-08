ec10de1b49235feed587776081de4fe13bfa9f80
Charles Grigg <charles.grigg2@gmail.com>
2021-12-07 21:00:20 -0500

    Day selection through CLI - post day 7

---
+++ src/day/7/index.js
@@ -0,0 +1,42 @@
+﻿const fs = require('fs');
+const path = require('path');
+const { sum } = require('../../util');
+
+const file = path.join(__dirname, 'data.txt');
+const data = fs.readFileSync(file, 'utf-8').toString().split(',').map(n => +n);
+
+// Part 1
+const crabFuelMinimizer9000 = crabs => {
+  const max = Math.max.apply(null, crabs);
+  const fuelMap = Array.from({ length: max }, (_, p) => (
+    crabs.reduce((total, crab) => total + Math.abs(crab - p), 0)
+  ));
+
+  return Math.min.apply(null, fuelMap);
+}
+
+const part1 = crabs => () => crabFuelMinimizer9000(crabs);
+
+// Part 2
+const generateCrabFuelConsumption = (source, target) => {
+  const min = Math.min(source, target);
+  const max = Math.max(source, target);
+
+  return [...Array((max - min) + 1).keys()].reduce(sum);
+}
+
+const crabFuelMinimizer9001 = crabs => {
+  const max = Math.max.apply(null, crabs);
+  const fuelMap = Array.from({ length: max }, (_, p) => (
+    crabs.reduce((total, crab) => total + generateCrabFuelConsumption(crab, p), 0)
+  ));
+
+  return Math.min.apply(null, fuelMap);
+}
+
+const part2 = crabs => () => crabFuelMinimizer9001(crabs);
+
+module.exports = {
+  exercises: [part1([...data]), part2([...data])],
+  name: 'A Whale of a Tale: Crab Fuel for the Soul',
+};
---
+++ src/index.js
@@ -0,0 +1,34 @@
+﻿const colors = require('colors');
+const path = require('path');
+const prompt = require('prompt');
+
+colors.enable();
+
+const runExercise = (exercise, part) => {
+  console.time('elapsed');
+  const result = exercise();
+
+  console.log(`———⤛ Part ${part + 1} ⤜———`.blue);
+  console.log('result: ', result);
+  console.timeEnd('elapsed');
+}
+
+(function aoc() {
+  prompt.start();
+
+  prompt.get({ name: 'day', pattern: /^\d+/ }, ( err, { day }) => {
+    try {
+      const duration = '———\nduration'.green.bold;
+      const filePath = path.join(__dirname, `day/${day}`);
+      const { exercises, name = 'Advent of Code 2021' } = require(filePath);
+
+      console.log(name.trap, `[Day ${day}]`);
+
+      console.time(duration);
+      exercises.forEach(runExercise);
+      console.timeEnd(duration);
+    } catch (error) {
+      console.error('Huh? Something happened!\n'.brightRed.bold, error.message);
+    }
+  });
+})();
